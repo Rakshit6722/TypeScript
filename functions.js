fd247aa8827ab14c173ed5c4de0565a5d17d7137
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-09 18:59:21 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,32 @@
+//simple function
+function func1() {
+    console.log('func1');
+}
+func1();
+//function with parameter and its type
+function func2(name) {
+    console.log('Hello ' + name);
+}
+func2('Rakshit');
+//function with parameter and return statement and its type
+function func3(a, b) {
+    return a + b;
+}
+console.log(func3(10, 20));
+//default value
+function func4(name, age) {
+    if (age === void 0) { age = 10; }
+    console.log(name, age);
+}
+func4('Rakshit', 40);
+//optional parameter
+function func5(name, age) {
+    console.log(name, age ? age : "");
+}
+func5('Rakshit');
+//multiply two number and return the result, also add an optional parameter to multiply the result by a factor
+function multiply(a, b, factor) {
+    var result = a * b;
+    return factor ? result * factor : result;
+}
+console.log(multiply(10, 20, 10));
