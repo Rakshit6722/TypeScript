6cc6992b176582ee724b958834c3355b27549a1b
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-09 19:07:30 +0530

    modify variable declaration

--- variableDeclaration.ts
+++ variableDeclaration.ts
@@ -1,7 +1,10 @@
 let name1:string = "Rakshit"
 let age:number = 20
 let isStudent:boolean = true
-
+let anything:undefined = undefined
+let noValue:void
+let anyvalue:any = "Hello"
 console.log(name1)
 console.log(age)
 console.log(isStudent)
+console.log(anything)
