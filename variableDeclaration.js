7fb595e60cc60cb39bca5ff69afcd457cdaa2369
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-09 18:43:07 +0530

    variable declaraion

---
+++ variableDeclaration.js
@@ -0,0 +1,6 @@
+var name1 = "Rakshit";
+var age = 20;
+var isStudent = true;
+console.log(name1);
+console.log(age);
+console.log(isStudent);
---
+++ variableDeclaration.ts
@@ -0,0 +1,7 @@
+let name1:string = "Rakshit"
+let age:number = 20
+let isStudent:boolean = true
+
+console.log(name1)
+console.log(age)
+console.log(isStudent)
