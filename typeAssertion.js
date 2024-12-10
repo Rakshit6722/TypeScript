5749af468f3b0aa93a50f4fcdc99cb72fafc60b6
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-10 19:14:29 +0530

    [Rakshit] Add: type assertion

---
+++ typeAssertion.js
@@ -0,0 +1,11 @@
+//type assertion
+var value = "Hello";
+var strLen = value.length;
+console.log(strLen);
+var value2 = 25.5;
+var num = Math.floor(value2);
+console.log(num);
+//NOTE -> 
+//differnce between any and unknown
+//any -> can be assigned to any type without any errors or type safety checks
+//unknown -> can be assigned to any type but with type safet checks
---
+++ typeAssertion.ts
@@ -0,0 +1,15 @@
+//type assertion
+
+let value: unknown = "Hello"
+let strLen: number = (value as string).length
+console.log(strLen)
+
+let value2: unknown = 25.5
+let num: number = Math.floor(value2 as number)
+console.log(num)
+
+
+//NOTE -> 
+//differnce between any and unknown
+//any -> can be assigned to any type without any errors or type safety checks
+//unknown -> can be assigned to any type but with type safet checks
