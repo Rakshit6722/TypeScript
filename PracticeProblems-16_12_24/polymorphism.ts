df306a1021b445856dea0760d1380449f885183c
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-16 18:26:52 +0530

    [Rakshit] Add: polymorphism

--- PracticeProblems-16_12_24/polymorphism.ts
+++ PracticeProblems-16_12_24/polymorphism.ts
@@ -37,3 +37,25 @@ let cat1 = new Cat1("Tom");
 
 describleAnimal(dog1);
 describleAnimal(cat1);
+
+abstract class Shape{
+    abstract draw(): void
+}
+
+class Circle extends Shape{
+    draw(){
+        console.log("Draw a circle");
+    }
+}
+
+class Rectangle extends Shape{
+    draw(){
+        console.log("Draw a rectangle");
+    }
+}
+
+let circle = new Circle()
+let rectangle = new Rectangle()
+
+circle.draw()
+rectangle.draw()
