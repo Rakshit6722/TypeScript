545c01de025f8db8ac31a732c528bcc0c8ab5b97
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-16 18:02:09 +0530

    rename folder

--- PracticeProblems-12_16_24/variableDeclaration.ts
+++
@@ -1,9 +0,0 @@
-function func(param: string|number):number {
-    if(typeof param === "string"){
-        return param.length
-    }
-    return param**2
-}
-
-console.log(func("Hello"))
-console.log(func(10))
