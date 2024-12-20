8f42fb5847bdfc85ee4ae2b8afbb4b6dc64895ea
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-20 15:29:18 +0530

    [Rakshit] Refactor: process different payments

--- PracticeProblems-20_12_24/polymorphism.ts
+++ PracticeProblems-20_12_24/polymorphism.ts
@@ -1,46 +1,82 @@
-abstract class Appliance{
-    abstract turnOn(): void;
-    abstract turnOff(): void;
+// abstract class Appliance{
+//     abstract turnOn(): void;
+//     abstract turnOff(): void;
+// }
+
+// class WashingMachine extends Appliance{
+//     turnOn(): void{
+//         console.log("Washing machine turned on")
+//     }
+
+//     turnOff(): void{
+//         console.log("Washing machine turned off")
+//     }
+// }
+
+// class AirConditioner extends Appliance{
+//     turnOn(): void{
+//         console.log("AC turned on")
+//     }
+
+//     turnOff(): void{
+//         console.log("AC turned off")
+//     }
+// }
+
+// class Refrigerator extends Appliance{
+//     turnOn(): void{
+//         console.log("Refrigerator turned on")
+//     }
+
+//     turnOff(): void{
+//         console.log("Refrigerator turned off")
+//     }
+// }
+
+// let washingMachine = new WashingMachine();
+// washingMachine.turnOn();
+// washingMachine.turnOff();
+
+// let airConditioner = new AirConditioner();
+// airConditioner.turnOn();
+// airConditioner.turnOff();
+
+// let refrigerator = new Refrigerator();
+// refrigerator.turnOn();
+// refrigerator.turnOff();
+
+abstract class Payments{
+    abstract processPayment(): void
 }
 
-class WashingMachine extends Appliance{
-    turnOn(): void{
-        console.log("Washing machine turned on")
-    }
-
-    turnOff(): void{
-        console.log("Washing machine turned off")
+class CreditCardPayment extends Payments{
+    processPayment(): void{
+        console.log("Credit card payment processed")
     }
 }
 
-class AirConditioner extends Appliance{
-    turnOn(): void{
-        console.log("AC turned on")
-    }
-
-    turnOff(): void{
-        console.log("AC turned off")
+class UPIPayment extends Payments{
+    processPayment(): void{
+        console.log("UPI payment processed")
     }
 }
 
-class Refrigerator extends Appliance{
-    turnOn(): void{
-        console.log("Refrigerator turned on")
-    }
-
-    turnOff(): void{
-        console.log("Refrigerator turned off")
+class WalletPayment extends Payments{
+    processPayment(): void{
+        console.log("Wallet payment processed")
     }
 }
 
-let washingMachine = new WashingMachine();
-washingMachine.turnOn();
-washingMachine.turnOff();
+let creditCardPayment = new CreditCardPayment();
+let upiPayment = new UPIPayment();
+let walletPayment = new WalletPayment();
 
-let airConditioner = new AirConditioner();
-airConditioner.turnOn();
-airConditioner.turnOff();
+let arr: Payments[] = [creditCardPayment, upiPayment, walletPayment]
 
-let refrigerator = new Refrigerator();
-refrigerator.turnOn();
-refrigerator.turnOff();
+function processPayments(arr: Payments[]): void{
+    arr.forEach(payment => {
+        payment.processPayment()
+    })
+}
+
+processPayments(arr)
